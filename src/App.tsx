b67e2aaadb9a556fbdf652880b6fcdb4c7935080
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-02-01 16:52:37 +0530

    added restai get user call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+import { Chat } from '@pushprotocol/uiweb';
+import * as PushApi from '@pushprotocol/restapi';
+
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 import './App.css';
 
+
 function App() {
+  
+  const { address, isConnected } = useAccount()
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+  const getUser = async() =>{
+    const response = await PushApi.user.get({
+      account: address!,
+      env:'staging',
+    });
+    console.log(response)
+  }
+  useEffect(() => {
+    getUser();
+  });
   return (
+   
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +41,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={() => connect()}>Connect Wallet</button>
+      <Chat
+      account = {address!}
+      supportAddress = "0x1aA3127b831a33830Ac0fE3ee4B0741d1A4a4E04"
+      env = 'staging'
+      apiKey="tAWEnggQ9Z.UaDBNjrvlJZx3giBTIQDcT8bKQo1O1518uF1Tea7rPwfzXv2ouV5rX9ViwgJUrXm"
+
+      />
     </div>
   );
 }
